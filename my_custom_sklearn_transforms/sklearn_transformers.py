f4b8dd3caa355b8bd979ccc14f7325ab1232a9f3
Igor Ventorim <igor.ventorim@gmail.com>
2020-08-24 20:35:51 -0300

    Adicionando encoding

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +15,21 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class EncodeColumns(BaseEstimator, TransformerMixin):
+
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+
+        for c in columns:
+            data[c] = data[c].astype('category').cat.codes
+
+        return data
